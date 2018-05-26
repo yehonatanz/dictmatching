87292dfe00c6b1513a10883c690ddbf07f7610b7
yehonatanz <yonti95@gmail.com>
2018-05-26 12:34:02 +0300

    Add pessimistic tests

--- tests/test_dictmatching.py
+++ tests/test_dictmatching.py
@@ -10,12 +10,18 @@ def d():
 
 @pytest.fixture
 def other(d):
-    return {k: -v for k, v in d.items()}
+    return dict({k: -v for k, v in d.items()}, c=True)
 
 
 def test_unpack_one(d):
     with unpack(d) as a:
         assert a == 3
+
+
+def test_unpack_non_existent(d):
+    with pytest.raises(KeyError):
+        with unpack(d) as non_existent:
+            pass
 
 
 def test_unpack_multi(d):
@@ -45,3 +51,12 @@ def test_unpack_several_dicts(d, other):
     with unpack(d) as (a, b), unpack(other) as b:
         assert a == 3
         assert b == -7
+    with unpack(d) as a, unpack(other) as c:
+        assert a == 3
+        assert c is True
+
+
+def test_unpack_several_dict_no_mix(d, other):
+    with pytest.raises(KeyError):
+        with unpack(d) as (a, c), unpack(other) as (a, c):
+            pass
