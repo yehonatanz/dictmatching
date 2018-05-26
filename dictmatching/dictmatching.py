71490a3e9f63b8e2d6bc23640d17f4ae342d7df4
yehonatanz <yonti95@gmail.com>
2018-05-26 12:08:52 +0300

    Test unpacking of several dicts at once

--- dictmatching/dictmatching.py
+++ dictmatching/dictmatching.py
@@ -17,23 +17,19 @@ def unpack(dictionary):
 
 
 def _get_with_destination(unpacker_frame):
-    line_ops = (
+    after_unpack = (
         op for op in dis(unpacker_frame.f_code)
-        if op.offset >= unpacker_frame.f_lasti
+        if op.offset >= unpacker_frame.f_lasti and op.name != 'EXTENDED_ARG'
     )
-    line_ops = list(line_ops)
     # Dispose SETUP_WITH
-    after_with = list(itertools.dropwhile(
-        lambda op: op.name != 'SETUP_WITH',
-        line_ops
-    ))[1:]
-    if after_with[0].name == 'STORE_FAST':
+    after_unpack = list(after_unpack)[1:]
+    if after_unpack[0].name == 'STORE_FAST':
         # A single variable unpacking
-        return after_with[0].desc
+        return after_unpack[0].desc
     else:
         # Multi variables unpacking, dispose of UNPACK_SEQUENCE
         store_ops = itertools.takewhile(
             lambda op: op.name == 'STORE_FAST',
-            after_with[1:]
+            after_unpack[1:]
         )
         return tuple(store_op.desc for store_op in store_ops)
--- tests/test_dictmatching.py
+++ tests/test_dictmatching.py
@@ -8,6 +8,11 @@ def d():
     return dict(a=3, b=7)
 
 
+@pytest.fixture
+def other(d):
+    return {k: -v for k, v in d.items()}
+
+
 def test_unpack_one(d):
     with unpack(d) as a:
         assert a == 3
@@ -31,3 +36,12 @@ def test_unpack_multiline(d):
     ):
         assert a == 3
         assert b == 7
+
+
+def test_unpack_several_dicts(d, other):
+    with unpack(d) as a, unpack(other) as (b, a):
+        assert a == -3
+        assert b == -7
+    with unpack(d) as (a, b), unpack(other) as b:
+        assert a == 3
+        assert b == -7
